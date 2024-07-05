0636a371e0e3d2dad5b621dab39cedcb227d12c4
zsg <zhao15085@qq.com>
2024-07-05 17:45:53 +0800

    Bean生命周期-七步

---
+++ spring6-005-lifecycle/src/main/java/com/goal/spring6/bean/LogBeanPostProcessor.java
@@ -0,0 +1,20 @@
+package com.goal.spring6.bean;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class LogBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("执行BeanBefore方法");
+
+        return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("执行BeanAfter方法");
+
+        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+    }
+}
